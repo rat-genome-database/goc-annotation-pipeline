41dfe72f72330019008cbd3f07af6155df014ede
harika91 <harika91@users.noreply.github.com>
2019-03-28 11:16:21 -0500

    Changes to log

--- src/main/java/edu/mcw/rgd/Manager.java
+++ src/main/java/edu/mcw/rgd/Manager.java
@@ -150,16 +150,16 @@ public class Manager {
 
         logSkipped.info(" Summary Report \n");
         logSkipped.info(" Total Number of GO Annotations in RGD: " + annotations.size());
-        logSkipped.info(" \n Total Number of Annotations Sent to GO from RGD: " + filteredList.size() );
-        logSkipped.info(" \n Obsolete Annotations: " + obsolete );
-        logSkipped.info(" \n Not gene Annotations: " + notGene );
-        logSkipped.info(" \n NotForCuration Annotations: " + notForCuration );
-        logSkipped.info(" \n IEP and HEP Annotations: " + iepHep );
-        logSkipped.info(" \n No Data (ND) evidence code Annotations: " + ndAnnotations );
-        logSkipped.info(" \n IPI Annotations to Catalytic Terms: " + ipiInCatalytic );
-        logSkipped.info(" \n IC,IPI,IDA Annotations violating WITH field rule: " + icIpiIda  );
-        logSkipped.info(" \n IBA annotations from other sources: "+ ibaAnnot );
-        logSkipped.info(" \n IPI annotations to root terms with null WITH field: " + ipiAnnot  );
+        logSkipped.info(" Total Number of Annotations Sent to GO from RGD: " + filteredList.size() );
+        logSkipped.info(" Obsolete Annotations: " + obsolete );
+        logSkipped.info(" Not gene Annotations: " + notGene );
+        logSkipped.info(" NotForCuration Annotations: " + notForCuration );
+        logSkipped.info(" IEP and HEP Annotations: " + iepHep );
+        logSkipped.info(" No Data (ND) evidence code Annotations: " + ndAnnotations );
+        logSkipped.info(" IPI Annotations to Catalytic Terms: " + ipiInCatalytic );
+        logSkipped.info(" IC,IPI,IDA Annotations violating WITH field rule: " + icIpiIda  );
+        logSkipped.info(" IBA annotations from other sources: "+ ibaAnnot );
+        logSkipped.info(" IPI annotations to root terms with null WITH field: " + ipiAnnot  );
 
 
         FileReader fr=new FileReader(getGoaFile());
